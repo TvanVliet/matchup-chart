6e96d940ea7fd2fe99b45be7ea697c5a2cd2a162
Tim van Vliet <dev.timvanvliet@gmail.com>
2018-11-07 12:49:48 +0100

    Add Junit test to test Fighter Object

---
+++ src/test/java/matchup/chart/TestFighter.java
@@ -0,0 +1,14 @@
+package matchup.chart;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestFighter {
+
+	@Test
+	void testIfFighterObjectIsCreatedSuccessfully() {
+		
+	}
+
+}
